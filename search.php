eeafb11daf46e607ad81ba6e50b5655292ce7296
mmees12 <mees.matthew@gmail.com>
2016-04-26 09:01:52 -0400

    Update 2
    Created my search page.

--- search.php
+++ search.php
@@ -15,39 +15,25 @@
 	</head>
 		
 	<body>
-		<div class = "container">
-			<h2>Find your favorite Recipe</h2>
-			<form role=form">
-			<div class="form-group">
-			<label for ="search">Search: </label>
-			<input type="text" class="form-control" id="search">m>
-			</div>
-			
-			</form>
-		</div>
-		
-	<body style = "text-align: center;">
 		<form action = "recipe_search.php" method = "get">
-		Recipe Name: <input name = "recipe" type = "text />
-		<br/>
-		Search by: <select name = "by">
-			<option value = ""></option> 
-			<option value="by Recipe Name">by Recipe Name</option>
-			<option value="by Family Origin">by Family Origin</option>
-			<option value="by Style">by Style</option>
-			</select>
-		<div class="dropdown">
-    <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Tutorials
-    <span class="caret"></span></button>
-    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">HTML</a></li>
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">CSS</a></li>
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">JavaScript</a></li>
-      <li role="presentation" class="divider"></li>
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">About Us</a></li>
-    </ul>	
+			<div class = "container">
+				<h2>Find your favorite Recipe</h2>
+			  	<form role=form">
+					<div class="form-group">
+					  <label for ="search">Search: </label>
+					  <input type="text" class="form-control" name="search">
+					</div>
+					<div class="form-group">
+						<label for="sel1">Select list:</label>
+						<select class="form-control" name = "search_by">
+						  <option>by Recipe Name</option>
+						  <option>by Family Origin</option>
+						  <option>by Style</option>
+						</select>
+					</div>
 			
-			
+				</form>
+			</div>
 		</form>
 	</body>
 </html>
